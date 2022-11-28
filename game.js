dc57f829fb246b2b9b13a99093489f68be0f21f6
j <77177771+jumping283@users.noreply.github.com>
2022-11-28 15:36:59 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,25 @@
-var http = require('http');
+var app = require('http').createServer(createServer);
+var fs = require('fs'); 
+var url = require('url');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(9001);
+function createServer(req, res) {
+    var path = url.parse(req.url).pathname;
+    var fsCallback = function(error, data) {
+        if(error) throw error;
+
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+    }
+
+    switch(path) {
+        case '/game':
+            doc = fs.readFile(__dirname + '/game.html', fsCallback);
+        break;
+        default:
+            doc = fs.readFile(__dirname + '/index.html', fsCallback);
+        break;
+    }
+}
+
+app.listen(7463);
